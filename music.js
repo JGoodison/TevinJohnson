66e2e69953be01dea45eb1fc015e88be458e3c9c
janae-goodison <Janae@live.unc.edu>
2022-07-15 09:00:36 -0400

    Basic sturture for listen page

---
+++ music.js
@@ -0,0 +1,17 @@
+// var slides = document.querySelectorAll(".slide");
+// var buttons = document.querySelectorAll('.button');
+// let currentslide = 1;
+
+// //javascript for image slider manual navigation 
+// var manualNav = function(manual){
+//     slides[manual].classList.add("active");
+//     buttons[manual].classList.add("active");
+
+// }
+
+// buttons.forEach((button, i) => {
+//     button.addEventListener("click", () => {
+//         manualNav(i);
+//         currentslide = i;
+//     });
+// });
