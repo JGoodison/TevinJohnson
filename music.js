11556c8d568deae9417c6f594035efa78c6fd7a4
janae-goodison <Janae@live.unc.edu>
2022-07-30 10:06:10 -0400

    finished product without media queries or pop out

--- music.js
+++ music.js
@@ -1,17 +1,93 @@
-// var slides = document.querySelectorAll(".slide");
-// var buttons = document.querySelectorAll('.button');
-// let currentslide = 1;
+let text = document.getElementsByClassName("text");
+window.onload = function deleteText(){
+    
+    // let text3 = document.getElementsByClassName("text3");
+    // text2.style.opacity = 0;
+    // text3.style.opacity = 0;
+    text.style.display = "none";
+   }
 
-// //javascript for image slider manual navigation 
-// var manualNav = function(manual){
-//     slides[manual].classList.add("active");
-//     buttons[manual].classList.add("active");
+// function deleteText(){
+    
 
 // }
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// buttons.forEach((button, i) => {
-//     button.addEventListener("click", () => {
-//         manualNav(i);
-//         currentslide = i;
-//     });
-// });
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  
+}
+
+// function byeText(){
+//     let text2 = document.getElementsByClassName("text2");
+//     let text3 = document.getElementsByClassName("text3");
+//     text2.style.display="none";
+//     text3.style.display="none";
+// }
+
+
+// Listening button action
+// function listenFunction(){
+//     let listenBtn = document.getElementsByClassName('listenBtn');
+
+//     for(let i = 0; i < listenBtn.length; i++){
+//         if(n==1){
+//             listenBtn[0].style.opacity= "0";
+//             break;
+//         }
+//         else if(n>1){
+//             for(let last = i-1; last>=0; last--){
+//                 listenBtn[i].style.opacity='0';
+//                 listenBtn[last].style.opacity= "1";
+//             }
+//         }
+//     }
+//     listenBtn.style.opacity='0';
+    
+// }
+
+
+// function opacity(){
+//     let musicButton = document.getElementsByClassName("music-buttons");
+//     let gridItemButton = document.getElementsByClassName("grid__item");
+//     let listenButton = document.getElementsByClassName("listenBtn");
+    
+    
+// }
+
+// function showImage(){
+//     let listenBtn = document.getElementsByClassName("image");
+//     listenBtn.style.opacity="1";
+// }
+// let i = 0;
+// function listenFunction(n){
+//     if(image.className == "open"){
+//         image.className = "";
+//         listen-button.innerHTML = "LISTEN"; 
+//     }
+//     else{
+
+//     }
+
+    
+// }
+
